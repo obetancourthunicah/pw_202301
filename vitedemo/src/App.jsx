270a81d802f2c651ef5e543500e73108f8a7650a
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-03-07 19:53:01 -0600

    React + Vite 1

---
+++ vitedemo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home from './pages/Home/Home'
+function App() {
+
+  return (
+    <section className="App">
+      <h1>Mi Primer App de React</h1>
+      <Home />
+    </section>
+  )
+}
+
+export default App
---
+++ vitedemo/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = ()=>{
+  return (
+    <main>
+      <h1>Home</h1>
+      <p>Lorem ipsum adatd asdfko asdf sfff</p>
+    </main>
+  )
+}
+export default Home;
