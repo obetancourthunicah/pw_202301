360657b421851928e90701f2a36f7c56fc4c9b50
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-03-13 19:47:19 -0600

    Import JSON

--- vitedemo/src/pages/About/About.jsx
+++ vitedemo/src/pages/About/About.jsx
@@ -1,8 +1,20 @@
+import AbouInfo from './AboutInfo.json';
 const About = () => {
   return (
     <section className="About">
       <h1>About</h1>
       <p>Lorem ipsum adatd asdfko asdf sfff</p>
+      {AbouInfo.map(
+        (item) => {
+          return (
+           <section key={item.nombre + item.apellido}>
+              <h2>{item.nombre} {item.apellido}</h2>
+              <p>{item.edad}</p>
+              <p>{item.hobbies.join(' | ')}</p>
+           </section>
+          );
+        }
+      )}
     </section>
   );
 }
