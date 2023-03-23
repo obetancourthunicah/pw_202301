1e11f5d43b07441faf99e343471e3094f5b37f78
Orlando J Betancourth <orlando.betancourth@gmail.com>
2023-03-22 19:56:09 -0600

    Pokemon API

--- vitedemo/src/pages/Pokemon/Pokemon.jsx
+++ vitedemo/src/pages/Pokemon/Pokemon.jsx
@@ -1,10 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getPokemonCards } from './PokemonService';
+
 export const Pokemon = () => {
-  const [counter, setCounter] = useState(0);
+  const [pokeCards, setPokeCards] = useState([]);
+  const [serviceState, setServiceState] = useState('init');
+  const [serviceError, setServiceError] = useState('');
+  const [page, setPage] = useState('');
+
+  useEffect(
+    ()=>{
+      setServiceState('loading');
+      getPokemonCards(page).then(
+        (pokeCardsApi)=>{
+          setServiceState('loaded');
+          setPokeCards([...pokeCards, ...pokeCardsApi.results]);
+        }
+      ).catch(()=>{
+        setServiceState('hasErrors');
+        setServiceError("Error al Cargar Poke Cards");
+      });
+    }
+    , [page]
+  );
 
   return (
     <div>
-      <h1 onClick={()=>{setCounter(counter + 1)}}>Pokemon {counter}</h1>
+      Service {serviceState}
+      <br />
+      Error {serviceError}
+      <br />
+      <hr />
+      <ol>
+      {pokeCards.map((o)=>{
+        return (
+          <li key={o.cardId}>{o.cardNumber} - {o.name} - {o.superType}</li>
+        );
+      })}
+      <a onClick={()=>{setPage(pokeCards[pokeCards.length-1].cardId)}} >Next</a>
+      </ol>
     </div>
   );
 };
---
+++ vitedemo/src/pages/Pokemon/PokemonService.js
@@ -0,0 +1,27 @@
+export const getPokemonCards = async (page) => {
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ead6b9fb3msh90d4404a2b71121p1b963bjsncf4dad082546',
+		'X-RapidAPI-Host': 'pokemon-tcg-card-prices.p.rapidapi.com'
+	}
+};
+try {
+  const url = page !== ""?
+    `https://pokemon-tcg-card-prices.p.rapidapi.com/card?setId=33ee55f4-30d0-4900-850f-36a351fb9719&set=vivid-voltage&series=sword-and-shield&limit=25&fromId=${page}`:
+    'https://pokemon-tcg-card-prices.p.rapidapi.com/card?setId=33ee55f4-30d0-4900-850f-36a351fb9719&set=vivid-voltage&series=sword-and-shield&limit=25';
+const response = await fetch(url, options)
+const jsonResponse = await response.json();
+return jsonResponse;
+}catch(ex){
+  console.log(ex)
+  return {
+    results: [],
+    paging:{
+      pageIndex:0,
+      pageSize:25,
+      count:0
+    }
+  }
+}
+}
